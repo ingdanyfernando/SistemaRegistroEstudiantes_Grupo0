3f2c785f767069711825c7a279112abefd9993ea
ingdanyfernando <ing.danyfernando@gmail.com>
2026-04-18 10:49:32 -0600

    He creado los paquetes y la clase MAIN

--- mavenproject1/src/main/java/com/tuusuario/examen/app/Main.java
+++ mavenproject1/src/main/java/com/tuusuario/examen/app/Main.java
@@ -11,5 +11,6 @@ package com.tuusuario.examen.app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Comenzando desde LA CLASE MAIN!");
+        /*aquí me equivoqué y lo subí a otro repositorio*/
     }
 }
