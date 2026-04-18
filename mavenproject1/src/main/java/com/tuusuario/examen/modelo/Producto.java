7faadf8a869f8c877f94f7356e6cfd63af087bd5
ingdanyfernando <ing.danyfernando@gmail.com>
2026-04-18 11:10:01 -0600

    He finalizado la clase Producto

---
+++ mavenproject1/src/main/java/com/tuusuario/examen/modelo/Producto.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tuusuario.examen.modelo;
+
+/**
+ *
+ * @author dmiranda
+ */
+public class Producto {
+private String nombre;
+private double precio;
+private int stock;
+
+
+    public Producto(String nombre, double precio, int stock) {
+        if(nombre == null || nombre.isEmpty())
+            throw new IllegalArgumentException("el nombre es OBLIGATORIO");
+        this.nombre = nombre;
+        
+        if(precio<0)
+            throw new IllegalArgumentException("No se aceptan valores NEGATIVSO");
+        this.precio = precio;
+            
+        if(stock < 0)
+            throw new IllegalArgumentException("Stok no puede ser NEGATIVO");
+            
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+    
+    public boolean hayStock()
+    {
+       return  stock > 0; 
+    }
+    
+    
+
+    
+}
